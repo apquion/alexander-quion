505d4957705779032daa5e4f3cae40867a96d76c
Alexander Philip Quion <alexq.ph@gmail.com>
2017-08-10 14:28:28 -0400

    Added single page navigation

---
+++ README.txt
@@ -0,0 +1,5 @@
+My Personal Website 
+
+A Pen created at CodePen.io. You can find this one at http://codepen.io/apquion/pen/jLMaXv.
+
+ 
--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
-$(document).ready(function(){
-  $('#menu').onePageNav();
-});
+$('a[href^="#"]').on('click',function(event)){
+  var target = $(this.getAttribute('href'));
+  if(target.length){
+    event.preventDefault();
+    $('html,body').stop().animate({
+      scrollTop: target.offset().top
+    },2000);
+  }
+}
